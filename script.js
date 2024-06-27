d558170acf7cec37ada993cfed8c845e94d9fe8d
ManikandanSridharan <smanikandan311099@gmail.com>
2024-06-27 13:32:58 +0530

    update project

---
+++ script.js
@@ -0,0 +1,52 @@
+document.getElementById('student-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var age = document.getElementById('age').value;
+   var genderElements = document.getElementsByName('gender');
+    var gender = '';
+    for (var i = 0; i < genderElements.length; i++) {
+        if (genderElements[i].checked) {
+            gender = genderElements[i].value;
+            break;
+        }
+     else {
+         alert("check gender")
+      }
+     }
+   // var gender = document.querySelector('input[name="gender"]:checked');
+    // if (gender) {
+    //     gender = gender.value;
+    // } else {
+    //     alert("check gender")
+    // }
+
+    var course = document.getElementById('select').value;
+    var email = document.getElementById('email').value;
+
+    var row = document.createElement('tr');
+    row.innerHTML = `
+        <td>${name}</td>
+        <td>${age}</td>
+        <td>${course}</td>
+        <td>${gender}</td>
+        <td>${email}</td>
+        <td><button class="delete-btn" onclick="deleteRow(this)">Delete</button></td>
+    `;
+    document.getElementById('student-rows').appendChild(row);
+
+    // Clear form inputs
+    document.getElementById('name').value = '';
+    document.getElementById('age').value = '';
+    document.getElementById('select').value = '';
+    var genderElements = document.getElementsByName('gender');
+    for (var i = 0; i < genderElements.length; i++) {
+        genderElements[i].checked = false;
+    }
+    document.getElementById('email').value = '';
+});
+
+function deleteRow(btn) {
+    var row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
